080d0a26fc11915288555082c42565a151069bee
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-03-12 12:25:07 +0900

    taskモデル作成　データ紐づきOK　new機能作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,6 +9,7 @@ class BoardsController < ApplicationController
   end
  
   def show
+    @tasks = @board.tasks
   end
 
   def new
---
+++ db/migrate/20210312010124_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :content
+      t.datetime :deadline
+
+      t.timestamps
+    end
+  end
+end
